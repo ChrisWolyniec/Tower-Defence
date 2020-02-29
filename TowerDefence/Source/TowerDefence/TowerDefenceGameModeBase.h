a934137bd744abaa7079f7a22a9f799ff5731fa2
Christopher Wolyniec <w014100h@student.staffs.ac.uk>
2020-02-29 19:41:20 +0000

    Initial commit

---
+++ TowerDefence/Source/TowerDefence/TowerDefenceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerDefenceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENCE_API ATowerDefenceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
